83b385728f92a809217674ec55124c622f756282
CortezWalter <cortezwaltera@gmail.com>
2023-07-30 22:14:01 -0300

    Style de carro y cards
    Se hace el estilo del carro para desktop y responsive.
    Se arreglas cosas resposive de las cards.

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import carro from "../imagenes/carro.png";
+import tachito from "../imagenes/tachito_blanco.png";
+import axios from "axios";
 import "../style/cart.css";
-import { Navbar } from "react-bootstrap";
 
 const Cart = ({
   allProducts,
@@ -17,6 +18,26 @@ const Cart = ({
   setCount,
 }) => {
   const [show, setShow] = useState(false);
+  const [resultPrice, setResultPrice] = useState (0);
+  const navigate = useNavigate();
+
+  const createPreference = async () => {
+    try {
+      const response = await axios.post("/create_preference", {
+        description: allProducts[0].name,
+        price: allProducts[0].price,
+        quantity: count,
+      });
+      return response.data.id;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleBuy = async () => {
+    const id = await createPreference();
+    navigate(`/pagos/${id}`);
+  };
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -25,6 +46,7 @@ const Cart = ({
   const addQuantity = (product) => {
     const f = allProducts.find((item) => item._id === product._id);
     if (f.quantity > 0 && count < f.quantity) {
+      setResultPrice(f.price + f.price);
       setCount(count + 1);
       setTotal(total + f.price);
       setCountProducts(countProducts + 1);
@@ -59,7 +81,7 @@ const Cart = ({
   return (
     <>
       <Button variant="primary" onClick={handleShow} className="cartButton">
-        <img src={carro} alt="" className="carro"/>
+        <img src={carro} alt="" className="carro_icon" />
       </Button>
       <Offcanvas show={show} onHide={handleClose} placement="end">
         <Offcanvas.Header closeButton>
@@ -70,33 +92,45 @@ const Cart = ({
             {allProducts.length > 0 ? (
               allProducts.map((product, id) => (
                 <div key={id}>
-                  <div>
-                    <img src={product.image} alt="imagen de producto" />
-                    <h2>{product.name}</h2>
-                    <button onClick={() => addQuantity(product)}>+</button>
-                    <div>{count}</div>
-                    <button onClick={() => restQuantity(product)}>-</button>
-                    <span>AR${product.price}</span>
-                    <button onClick={() => deleteProduct(product)}>
-                      Tachito
-                    </button>
-                    <button onClick={emptyCart}>Vaciar carrito</button>
+                  <div className="carro_body">
+                    <div className="carro_1ra">
+                      <div className="carro-1ra_img">
+                        <img src={product.image} alt="imagen de producto" className="img-carro"/>
+                      </div>
+                      <div className="carro-1ra_product">
+                        <h2 className="carro-1ra_product__title">{product.name}</h2>
+                        <span className="carro-1ra_product__price">AR${product.price}</span>
+                        <div className="carro-1ra_counter">
+                          <button className="boton_counter" onClick={() => restQuantity(product)}>-</button>
+                          <span className="counter">{count}</span>
+                          <button className="boton_counter" onClick={() => addQuantity(product)}>+</button>
+                        </div>
+                      </div>
+                      <div className="carro-1ra_subtotal">
+                        <span>Subtotal: {resultPrice} </span>
+                        <button className="boton-counter_subtotal" onClick={() => deleteProduct(product)}>
+                          <img src={tachito} alt="" className="img-tachito"/>
+                        </button>
+                      </div>
+                    </div>
+                    <hr />
                   </div>
                 </div>
               ))
             ) : (
               <p>El carrito está vacío</p>
             )}
-            <div>Total de productos: {countProducts}</div>
-            <div>${total}</div>
-            <Link to="/pagos">
+
+            {/*<Link to="/pagos">
               <button disabled={!allProducts.length > 0}>Ir a pagar</button>
-            </Link>
+            </Link>*/}
           </div>
+          <div>Total de productos: {countProducts}</div>
+          <div>${total}</div>
         </Offcanvas.Body>
       </Offcanvas>
     </>
   );
 };
 
-export default Cart;
+export default Cart;
