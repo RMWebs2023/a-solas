0a524d43c2d9d1fcc3cfeb81ffa3a43b88f36ede
René Valderrey González <renevalderrey@outlook.com>
2023-08-08 18:39:38 -0300

    admin

--- client/src/components/CreateProduct.jsx
+++ client/src/components/CreateProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { postProducts } from "../redux/action";
-import "../style/admin.css"
+import "../style/admin.css";
 
 const Modal = ({ show, close }) => {
   const dispatch = useDispatch();
@@ -87,44 +87,90 @@ const Modal = ({ show, close }) => {
           <header>
             <h2 className="adm-add_title"> Agregar un producto </h2>
           </header>
-          
+
           <form className="adm-form_add" onSubmit={(e) => onSubmit(e)}>
-            <label className="form-label"> Nombre:
-              <input placeholder="Por ejemplo: vibrador de silicona..." value={name} onChange={(e) => inputName(e)} />
+            <label className="form-label">
+              {" "}
+              Nombre:
+              <input
+                placeholder="Por ejemplo: vibrador de silicona..."
+                value={name}
+                onChange={(e) => inputName(e)}
+              />
             </label>
-            <label className="form-label"> Precio:
+            <label className="form-label">
+              {" "}
+              Precio:
               <input value={price} onChange={(e) => inputPrice(e)} />
             </label>
-            <label className="form-label"> Categoría:
-              <input placeholder="Por ejemplo: Vibrador" value={category} onChange={(e) => inputCategory(e)} />
-            </label>
-            <label className="form-label"> Subcategoría:
-              <input placeholder="Por ejemplo: silicona" value={subcategory} onChange={(e) => inputSubcategory(e)}
+            <label className="form-label">
+              {" "}
+              Categoría:
+              <input
+                placeholder="Por ejemplo: Vibrador"
+                value={category}
+                onChange={(e) => inputCategory(e)}
               />
             </label>
-            <label className="form-label"> Detalle:
-              <input placeholder="Detalle" value={details} onChange={(e) => inputDetails(e)} />
-            </label>
-            <label className="form-label"> Descripción:
-              <input placeholder="Descripcion del producto" value={description} onChange={(e) => inputDescription(e)}
+            <label className="form-label">
+              {" "}
+              Subcategoría:
+              <input
+                placeholder="Por ejemplo: silicona"
+                value={subcategory}
+                onChange={(e) => inputSubcategory(e)}
               />
             </label>
-            <label className="form-label"> Imagen:
-              <input placeholder="Insertar imagen del producto" value={image} onChange={(e) => inputImage(e)} />
+            <label className="form-label">
+              {" "}
+              Detalle:
+              <input
+                placeholder="Detalle"
+                value={details}
+                onChange={(e) => inputDetails(e)}
+              />
             </label>
-            <label className="form-label"> Cantidad:
+            <label className="form-label">
+              {" "}
+              Descripción:
+              <input
+                placeholder="Descripcion del producto"
+                value={description}
+                onChange={(e) => inputDescription(e)}
+              />
+            </label>
+            <label className="form-label">
+              {" "}
+              Imagen:
+              <input
+                placeholder="Insertar imagen del producto"
+                value={image}
+                onChange={(e) => inputImage(e)}
+              />
+            </label>
+            <label className="form-label">
+              {" "}
+              Cantidad:
               <input value={quantity} onChange={(e) => inputQuantity(e)} />
-            </label>            
+            </label>
           </form>
 
           <footer className="footer-form">
-              <button className="boton-form" submit="submit">Crear</button>
-              <button className="boton" onClick={() => {
-                  close();
-                }}
-              >
-                Cancelar creación
-              </button>
+            <button
+              className="boton-form"
+              submit="submit"
+              onClick={(e) => onSubmit(e)}
+            >
+              Crear
+            </button>
+            <button
+              className="boton"
+              onClick={() => {
+                close();
+              }}
+            >
+              Cancelar creación
+            </button>
           </footer>
         </div>
       ) : null}
