d5594263a0bfad0cb913930b7d4e7c0e635de824
CortezWalter <cortezwaltera@gmail.com>
2023-09-12 22:00:08 -0300

    cambios de css
    cambios de css

--- client/src/components/Comercial.jsx
+++ client/src/components/Comercial.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../style/comercial.css";
 import camionEnvios from "../imagenes/camion_envios.png";
-import mediosDePago from "../imagenes/medios_pago.png";
+import mediosDePago from "../imagenes/mediosdepago-negro.png";
 import wspNegro from "../imagenes/logos/wsp_negro.png";
 
 const Comercial = () => {
@@ -10,27 +10,28 @@ const Comercial = () => {
       <div className="back-2parte">
 
         <div className="comercial">
-          <div className="envios">
+          <div className="comercial_card">
             <img src={camionEnvios} alt="" />
             <h3>Envios a todo el pais</h3>
           </div>
-          <p>Envios con Correo Argentino empresa - a cargo del comprador</p>
+          <p>Envios con X empresa - a cargo del comprador</p>
         </div>
 
         <div className="comercial">
-          <div className="pagos">
-            <img src={mediosDePago} alt="" />
-            <h3>Aceptamos tarjetas de credito/debito y transferencia bancaria</h3>
+          <div className="comercial_card">
+            <img src={mediosDePago} alt="" className="pagos_img"/>
+            <h3>Aceptamos tarjetas de débito y crédito</h3>
           </div>
           <p>Gestionados por Mercado Pago</p>
         </div>
 
         <div className="comercial">
-          <div className="wsp">
-            <a href="https://api.whatsapp.com/send?phone=5492615618566" className="url_wsp" target="_blank">
-            <img src={wspNegro} alt="" />
+          <div className="comercial_card">
+            <div>
+              <a href="https://api.whatsapp.com/send?phone=5492615618566" className="url_wsp" target="_blank">
+              <img src={wspNegro} alt="" /></a>
+            </div>
             <h3>Escribenos tu consulta</h3>
-            </a>
           </div>
           <p><a href="https://api.whatsapp.com/send?phone=5492615618566" className="url_wsp" target="_blank">+54 9 261 561-8566</a></p>
         </div>
