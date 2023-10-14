f68332a91cd50d354dd3c7141f31f54b5189ef3c
René Valderrey González <renevalderrey@outlook.com>
2023-10-14 10:48:42 -0300

    act

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://RMWebs2023.github.io/a-solas/",
+  base: "https://github.com/RMWebs2023/a-solas/tree/master/client"
+  // base: "https://RMWebs2023.github.io/a-solas/",
 });
